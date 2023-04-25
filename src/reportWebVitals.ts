266cc3f2581810864f9c1c79b2fa2c5c97b1ba69
Sergey_Rimsha <Sergey.rimsha.ru@gmail.com>
2023-04-25 23:00:50 +0300

    fix eslint

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -3,6 +3,7 @@ import { type ReportHandler } from 'web-vitals';
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry != null && onPerfEntry instanceof Function) {
+    // eslint-disable-next-line no-void
     void import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
